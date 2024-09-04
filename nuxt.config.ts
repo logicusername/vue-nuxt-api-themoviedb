53fb30a4e0983286780269d7eae354a31e8397e2
Diego Cruz <dcruzgr@gmail.com>
2024-09-04 00:16:18 -0500

    change nuxt

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,13 @@ import Aura from "@primevue/themes/aura";
 export default defineNuxtConfig({
   compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
+  nitro: {
+    baseURL: "http://localhost:8000/movies",
+    prerender: {
+      crawlLinks: true,
+      failOnError: false, 
+    },
+  },
   modules: ["@primevue/nuxt-module",'@nuxtjs/tailwindcss'],
   primevue: {
     options: {
